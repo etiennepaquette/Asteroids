7746f4a6b6d24cb4bee2b26a57941b6d211a739f
etiennepaquette <E_T2004@hotmail.com>
2019-10-26 23:16:59 -0400

    feat: Basic movement

---
+++ Asteroids/Assets/ShipMovements.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovements : MonoBehaviour
+{
+    public float speed = 10f;
+    public float turnSpeed = 200f;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        
+        rb.AddForce(transform.forward * verticalMovement * speed);
+        rb.rotation *= Quaternion.AngleAxis(turnSpeed * horizontalMovement * Time.deltaTime, Vector3.up);
+    }
+}
