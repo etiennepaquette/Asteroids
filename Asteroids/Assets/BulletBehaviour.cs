e9746f0901446e0159a8e5674ca14fe055a9a9aa
etiennepaquette <E_T2004@hotmail.com>
2019-10-27 21:43:31 -0400

    feat: Firing bullets

---
+++ Asteroids/Assets/BulletBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    Rigidbody rb;
+    public float bulletSpeed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, 5.0f);
+    }
+    
+    void Update()
+    {
+        rb.velocity = transform.forward * bulletSpeed;
+    }
+}
---
+++ Asteroids/Assets/Scripts/ShipAttacks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAttacks : MonoBehaviour
+{
+    public Transform bulletOrigin;
+    public GameObject prefBullet;
+    public float fireDelay = 1.0f;
+    public float lastFireTimestamp = 0.0f;
+    public Transform bulletsParent;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && (Time.time - lastFireTimestamp) > fireDelay) {
+            Instantiate(prefBullet, bulletOrigin.position, bulletOrigin.rotation, bulletsParent);
+            lastFireTimestamp = Time.time;
+        }
+    }
+}
